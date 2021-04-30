a0713f86a31840a760f2394e0d111d2e812bc90c
jumax108 <jumax108@kakao.com>
2021-05-01 02:55:50 +0900

    SimplePacker Change packing data architecture Add some error handling

--- SimplePacker/SimplePacker.cpp
+++ SimplePacker/SimplePacker.cpp
@@ -1,42 +1,52 @@
 ﻿#include <iostream>
-#include <string.h>
+#include <vector>
 using namespace std;
 
+#pragma pack(1)
 struct SFileHeader {
-	char* fileName;
+	char name[256];
 	unsigned int size;
-	unsigned char fileNameLength;
 };
+#pragma pack()
 
 void packing() {
 	FILE* fpFileList = nullptr;
-	char fileList[100][255] = { 0, };
-	char* buffer;
+	vector<char*> fileList;
+	char* buffer = nullptr;
 
 	fopen_s(&fpFileList, "filelist.txt", "rb");
 	if (fpFileList == nullptr) {
-		cout << "filelist.txt를 찾을 수 없습니다.";
+		cout << "Cannot open filelist.txt";
 		return ;
 	}
 	fseek(fpFileList, 0, SEEK_END);
 	int size = ftell(fpFileList);
 	rewind(fpFileList);
 
-	size += 1;
-	buffer = (char*)malloc(size);
-	fread(buffer, 1, size - 1, fpFileList);
+	buffer = (char*)malloc(size + 1);
+	if (buffer == nullptr) {
+		cout << "Malloc Error";
+		return;
+	}
+	fread(buffer, 1, size, fpFileList);
 	fclose(fpFileList);
-	buffer[size - 1] = '\0';
+	buffer[size] = '\0';
 
 	int fileNum = 0;
 	char* fileNameBegin = &buffer[0];
-	for (int bufCnt = 0; bufCnt < size; ++bufCnt) {
+	for (int bufCnt = 0; bufCnt <= size; ++bufCnt) {
 		switch (buffer[bufCnt]) {
 		case '\0':
 		case 0x0D:
 
 			if (&buffer[bufCnt] - fileNameBegin == 0) {
 				break;
+			}
+
+			fileList.push_back((char*)malloc(256));
+			if (fileList[fileNum] == nullptr) {
+				cout << "Malloc Error";
+				return;
 			}
 
 			for (char* pBuf = fileNameBegin; pBuf != &buffer[bufCnt]; ++pBuf) {
@@ -47,12 +57,12 @@ void packing() {
 			break;
 		}
 	}
-
 	free(buffer);
 	
 	char** fileData = (char**)malloc(fileNum);
-	SFileHeader* fileHeader = (SFileHeader*)malloc(fileNum);
+	SFileHeader* fileHeader = (SFileHeader*)malloc(sizeof(SFileHeader) * fileNum);
 	if (fileHeader == nullptr) {
+		cout << "Malloc Error";
 		return;
 	}
 
@@ -61,7 +71,7 @@ void packing() {
 		int size = 0;
 		fopen_s(&fpFile, fileList[fileCnt], "rb");
 		if (fpFile == nullptr) {
-			cout << fileList[fileCnt] << ": 파일을 읽을 수 없습니다.\n";
+			cout << fileList[fileCnt] << ": Cannot open file.\n";
 			return;
 		}
 		fseek(fpFile, 0, SEEK_END);
@@ -69,35 +79,26 @@ void packing() {
 		rewind(fpFile);
 
 		fileData[fileCnt] = (char*)malloc(size);
-		if (fileData == nullptr) {
+		if (fileData[fileCnt] == nullptr) {
+			cout << "Malloc Error";
 			return;
 		}
 		fread(fileData[fileCnt], 1, size, fpFile);
 		fclose(fpFile);
-
-		fileHeader[fileCnt].fileNameLength = strlen(fileList[fileCnt]);
-		fileHeader[fileCnt].fileName = (char*)malloc(fileHeader[fileCnt].fileNameLength);
-		if (fileHeader[fileCnt].fileName == nullptr) {
-			return;
-		}
-		memcpy(fileHeader[fileCnt].fileName, fileList[fileCnt], fileHeader[fileCnt].fileNameLength);
-		cout << fileHeader[fileCnt].fileName << "\n";
 		fileHeader[fileCnt].size = size;
+		memcpy(fileHeader[fileCnt].name, fileList[fileCnt], 255);
 	}
 
 	FILE* fileOutput = nullptr;
 	fopen_s(&fileOutput, "output", "wb");
 	if (fileOutput == nullptr) {
+		cout << "Cannot make output file";
 		return;
 	}
 
 	fwrite(&fileNum, sizeof(fileNum), 1, fileOutput);
-
-	for (int fileCnt = 0; fileCnt < fileNum; ++fileCnt) {
-		fwrite(&(fileHeader[fileCnt].fileNameLength), sizeof(fileHeader[fileCnt].fileNameLength), 1, fileOutput);
-		fwrite(fileHeader[fileCnt].fileName, fileHeader[fileCnt].fileNameLength, 1, fileOutput);
-		fwrite(&(fileHeader[fileCnt].size), sizeof(fileHeader[fileCnt].size), 1, fileOutput);
-	}
+	
+	fwrite(fileHeader, sizeof(SFileHeader), fileNum, fileOutput);
 	
 	for (int fileCnt = 0; fileCnt < fileNum; ++fileCnt) {
 		fwrite(fileData[fileCnt], fileHeader[fileCnt].size, 1, fileOutput);
@@ -105,8 +106,12 @@ void packing() {
 
 	fclose(fileOutput);
 
-	free(fileData);
 	free(fileHeader);
+	for (int fileCnt = 0; fileCnt < fileNum; ++fileCnt) {
+		free(fileList[fileCnt]);
+		free(fileData[fileCnt]);
+	}
+	free(fileData);
 }
 
 void unpacking() {
@@ -123,15 +128,11 @@ void unpacking() {
 
 	fileHeader = (SFileHeader*)malloc(sizeof(SFileHeader) * fileNum);
 	if (fileHeader == nullptr) {
+		cout << "Malloc Error";
 		return;
 	}
 
-	for (int fileCnt = 0; fileCnt < fileNum; ++fileCnt) {
-		fread(&(fileHeader[fileCnt].fileNameLength), sizeof(fileHeader[fileCnt].fileNameLength), 1, packedFile);
-		fileHeader[fileCnt].fileName = (char*)malloc(fileHeader[fileCnt].fileNameLength);
-		fread(fileHeader[fileCnt].fileName, fileHeader[fileCnt].fileNameLength, 1, packedFile);
-		fread(&(fileHeader[fileCnt].size), sizeof(fileHeader[fileCnt].size), 1, packedFile);
-	}
+	fread(fileHeader, sizeof(SFileHeader), fileNum, packedFile);
 
 	fileData = (char**)malloc(fileNum);
 	if (fileData == nullptr) {
@@ -141,14 +142,18 @@ void unpacking() {
 
 	for (int fileCnt = 0; fileCnt < fileNum; ++fileCnt) {
 		fileData[fileCnt] = (char*)malloc(fileHeader[fileCnt].size);
+		if (fileData[fileCnt] == nullptr) {
+			cout << "Malloc Error";
+			return;
+		}
 		fread(fileData[fileCnt], fileHeader[fileCnt].size, 1, packedFile);
 		FILE* makeFile = nullptr;
-		char* fileName = (char*)malloc(fileHeader[fileCnt].fileNameLength + 1);
-		memcpy(fileName, fileHeader[fileCnt].fileName, fileHeader[fileCnt].fileNameLength);
-		fileName[fileHeader[fileCnt].fileNameLength] = '\0';
+		char fileName[257] = {0,};
+		memcpy(fileName, fileHeader[fileCnt].name, 255);
 		fopen_s(&makeFile, fileName, "wb");
 		if (makeFile == nullptr) {
-			cout << fileHeader[fileCnt].fileName << ": 파일을 열 수 없습니다.";
+			cout << fileHeader[fileCnt].name << ": 파일을 열 수 없습니다.";
+			break;
 		}
 		fwrite(fileData[fileCnt], fileHeader[fileCnt].size, 1, makeFile);
 		fclose(makeFile);
@@ -171,9 +176,6 @@ int main() {
 		unpacking();
 		break;
 	}
-
-
-
 
 	return 0;
 }
